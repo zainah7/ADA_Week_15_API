f0ccb39dc3563c92c646dc15b3415077c4f8442a
zainah7 <zainaabd7@gmail.com>
2023-01-27 01:09:06 +0100

    Tell A Joke

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-     let setup = document.getElementById("setup");
-     let punchlinebtn = document.getElementById("punclinebtn");
-     let punchline = document.getElementsByClassName("punchline");
-     let jokeimg = document.getElementById("joke-img");
-     let resetBtn = document.getElementById("reset")
+let setup = document.getElementById("setup");
+let punchlinebtn = document.getElementById("punchlinebtn");
+let punchline = document.querySelector(".punchline");
+let jokeimg = document.getElementById("joke-img");
+let resetBtn = document.querySelector(".reset")
 
-     let joke = {};
-    
+let joke = {};
 
-     const tellAJoke = () => {
+const tellAJoke = () => {
+     punchline.style.display = "none";
+     jokeimg.src = "https://i.pinimg.com/originals/b2/bb/e8/b2bbe8c8f46afa220b048b97be926df3.gif"
      fetch("https://official-joke-api.appspot.com/random_joke")
      .then(res => res.json())
      .then(
@@ -18,25 +19,19 @@
           (joke = data)
        )
      )
-
      .catch(err => console.log(err));
      };
-
-
-     const tellPunchline = () =>{
-          punchline.innerHTML = joke.punchline;
-          jokeimg.src = "https://i.pinimg.com/564x/f0/05/39/f00539302feb966bfbf8530377d22bdb.jpg"
-          resetBtn.style.display = "block"
-
+    
+     const tellPunchline = () => {
+          punchline.style.display = "block";
+          jokeimg.src = 
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL3t67tm0PBNcfrGlunoXYPgUktBT2uREjZQ&usqp=CAU"
+          console.log(joke.punchline);
      };
-     
-     //reset button
 
-     const $ResetBtn = () =>{
-          setup.innerHTML = "";
-          punchlinebtn.style.display = "none";
-          // setup.innerHTML = data.setup;
-          punchline.innerHTML = "";
-          joke = {};
-          resetBtn.style.display = "none";
-       }
+     punchlinebtn.addEventListener("click", tellPunchline);
+
+     resetBtn.addEventListener("click", () => {
+          window.location.reload();
+
+     });
